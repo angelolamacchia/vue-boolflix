ee5ef2366edd66b43ecd3d6176be7226943e7014
Angelo Lamacchia <angelo.lamacchia@live.it>
2021-04-06 18:35:57 +0200

    milestone 2

--- js/script.js
+++ js/script.js
@@ -12,12 +12,16 @@ var root = new Vue ({
 
     methods: {
         sendData() {
-            axios
-            .get("https://api.themoviedb.org/3/search/movie?api_key=52dbf5b6e21f1268285391475335cb84&query=" + this.keyWord)
+            axios.get("https://api.themoviedb.org/3/search/movie?api_key=52dbf5b6e21f1268285391475335cb84&query=" + this.keyWord),
+            axios.get("https://api.themoviedb.org/3/search/tv?api_key=52dbf5b6e21f1268285391475335cb84&query=" + this.keyWord)
             .then( (result) =>{
                 console.log(result.data.results);
                 this.filmList = result.data.results;
+
+                this.filmList.vote_average = "prova";
             });
+
+            console.log(this.filmList.vote_average);
         },
     },
 
